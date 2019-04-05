66f0a780ea71a093e4c4879f3f03c71716c5e1bf
Benjamin Gwynn <benjamin@gwynn.design>
2019-04-05 02:31:22 +0100

    Export ReactionMode

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,9 @@
 import Activity from "./Activity"
 import Application from "./Application"
 import Component from "./Component"
-import ReactiveComponent from "./ReactiveComponent"
+import ReactiveComponent, {ReactionMode} from "./ReactiveComponent"
 import FrameComponent from "./FrameComponent"
 import * as DOMUtil from "./DOMUtil"
 import * as util from "./util"
 
-export {Application, Activity, Component, ReactiveComponent, FrameComponent, DOMUtil, util}
+export {Application, Activity, Component, ReactiveComponent, FrameComponent, DOMUtil, util, ReactionMode}
