19107719d28d941477be149ece2f4e22d770c847
Benjamin Gwynn <benjamin@gwynn.design>
2019-04-05 01:50:50 +0100

    Correctly @format util and add `toKebabCase` and `sleepFrames`

--- src/util.ts
+++ src/util.ts
@@ -1,9 +1,45 @@
-export function sleep (ms:number) {
-	return new Promise ((resolve) => {
+/** @format */
+
+/** Wait a number of seconds before resolving. */
+export function sleep(ms: number) {
+	return new Promise((resolve) => {
 		if (ms <= 0) {
 			resolve()
 		} else {
 			setTimeout(resolve, ms)
 		}
 	})
+}
+
+/** Wait a specified number of frames before resolving. */
+export function sleepFrames(nFrames: number) {
+	return new Promise((resolve) => {
+		let n = 0
+		const f = () => {
+			if (n++ === nFrames) {
+				resolve()
+			} else {
+				requestAnimationFrame(f)
+			}
+		}
+		requestAnimationFrame(f)
+	})
+}
+
+/** Converts an input to kebab-case */
+export function toKebabCase(input: string): string {
+	let output: string = ""
+	input.split("").forEach((char, index) => {
+		if (char.toUpperCase() === char) {
+			// this is an upper case character
+			if (index !== 0) {
+				output += "-"
+			}
+
+			output += char.toLowerCase()
+		} else {
+			output += char
+		}
+	})
+	return output
 }
