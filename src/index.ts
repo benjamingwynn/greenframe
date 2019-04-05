00b9b382491ced6c0b4f98709c93617163f1d024
Benjamin Gwynn <benjamin@gwynn.design>
2019-04-05 02:22:55 +0100

    Add index

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+/** @format */
+
+import Activity from "./Activity"
+import Application from "./Application"
+import Component from "./Component"
+import ReactiveComponent from "./ReactiveComponent"
+import FrameComponent from "./FrameComponent"
+import * as DOMUtil from "./DOMUtil"
+import * as util from "./util"
+
+export default {
+	Application,
+	Activity,
+	Component,
+	ReactiveComponent,
+	FrameComponent,
+	DOMUtil,
+	util,
+}
