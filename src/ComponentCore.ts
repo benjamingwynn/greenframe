d946d07626554f8ba133af3408082d32ce61d1df
Benjamin Gwynn <benjamin@gwynn.design>
2019-08-08 15:52:24 +0100

    add UID generation to Components

--- src/ComponentCore.ts
+++ src/ComponentCore.ts
@@ -441,4 +441,18 @@ export default abstract class ComponentCore extends HTMLElement {
 
 		this.connectedCallbackFinished = true
 	}
+
+	private _uid?: string
+	/** Get a unique ID for the component */
+	public get uid() {
+		return (
+			this._uid ||
+			(this._uid = (() =>
+				"xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, (c) => {
+					const r = (Math.random() * 16) | 0,
+						v = c == "x" ? r : (r & 0x3) | 0x8
+					return v.toString(16)
+				}))())
+		)
+	}
 }
